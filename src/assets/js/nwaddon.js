2bb4cea03b977fd32e12fa922381645382152dbd
Sasha Castillo <sasha.castillo@ucdenver.edu>
2016-07-29 16:32:57 -0600

    Universal List: 3 Concepts

--- src/assets/js/nwaddon.js
+++ src/assets/js/nwaddon.js
@@ -1,33 +1,33 @@
-  
+
 $(document).ready(function() {
     // $("#subgroup .accordion").foundation('down', $(".accordion .accordion-content"));
 
-    
+
     $("#btnOpen").click(function() {
         Foundation.Accordion.defaults.multiExpand = true;
 
-    
+
         //if (!$("#subgroup .accordion .accordion-item").hasClass("is-active")) {
-     
+
             $("#subgroup .accordion").foundation('down', $(".accordion .accordion-content"));
-            // $("#btnOpen").removeClass("accordionshow").addClass("accordionhide");   
+            // $("#btnOpen").removeClass("accordionshow").addClass("accordionhide");
         // } else {
         //     $("#subgroup .accordion").foundation('up', $(".accordion .accordion-content"));
         //     // $("#btnOpen").removeClass("accordionhide").addClass("accordionshow");
 
         //}
- 
+
     });
 
     $("#btnClose").click(function() {
 
-    
+
         //if ($("#subgroup .accordion .accordion-item").hasClass("is-active")) {
-     
+
             $("#subgroup .accordion").foundation('up', $(".accordion .accordion-content"));
 
         //}
- 
+
     });
 
     // $("#btnOpen").click(function() {
@@ -40,7 +40,7 @@ $(document).ready(function() {
         // } else {
         //     $("#btnOpen").removeClass("accordionhide").addClass("accordionshow");
         // }
-   
+
 
 
 
@@ -48,7 +48,7 @@ $(document).ready(function() {
     //database design show and hide//
     /////////////////////////////////////////////////
     //var foo = new Foundation.Accordion($("#foo"));
-  
+
 
 
 	//Configure/customize these variables.
@@ -56,23 +56,23 @@ $(document).ready(function() {
     var ellipsestext = "...";
     var moretext = "Show more >";
     var lesstext = "Show less";
-    
+
 
     $('.more').each(function() {
         var content = $(this).html();
- 
+
         if(content.length > showChar) {
- 
+
             var c = content.substr(0, showChar);
             var h = content.substr(showChar, content.length - showChar);
- 
+
             var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
- 
+
             $(this).html(html);
         }
- 
+
     });
- 
+
     $(".morelink").click(function(){
         if($(this).hasClass("less")) {
             $(this).removeClass("less");
@@ -85,5 +85,5 @@ $(document).ready(function() {
         $(this).prev().toggle();
         return false;
     });
-    
+
 });
