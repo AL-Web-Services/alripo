7986ec416fa9f434950d66ef5080c1655b454197
Sasha Castillo <sasha.castillo@ucdenver.edu>
2016-08-24 14:44:41 -0600

    jQuery Toggle v-1

--- src/assets/js/sasha.js
+++ src/assets/js/sasha.js
@@ -1,8 +1,39 @@
-$('.image-wrapper').click(function(e){
-   // e.preventDefault();
-    $('.image-overlay-content').fadeToggle();
-});
+//$('.image-wrapper').click(function(e){
+    //$('.image-overlay-content').fadeToggle();
+//});
 
 //$('.image-overlay-content').click(function(){
     //$(this).fadeOut();
 //});
+
+//$(window).resize(function(){
+//});
+
+
+  //if($(window).width() <= 1024) {
+  //  $('.image-wrapper').click(function(){
+  //      $('.image-overlay-content').fadeToggle();
+  //  });
+  //}
+  //else {
+  //  $('.image-wrapper').hover(function(){
+  //      $('.image-overlay-content').fadeToggle(1000);
+  //  });
+  //}
+
+  if($(window).width() <= 1024) {
+    $('.simple-toggle').click(function(){
+        $('.simple-toggle-content').fadeToggle();
+    });
+
+    //$('.simple-toggle').click(function(event){
+    //     event.stopPropagation();
+    //});
+
+  }
+  else {
+    $('.simple-toggle').hover(function(e){
+      e.preventDefault();
+        $('.simple-toggle-content').fadeToggle(1000);
+    });
+  }
