cd2b09fceb819156aae804bfb75e1a6e282110e3
Sasha Castillo <sasha.castillo@ucdenver.edu>
2016-08-24 18:50:53 -0600

    jQuery Toggle v-2

--- src/assets/js/sasha.js
+++ src/assets/js/sasha.js
@@ -1,3 +1,5 @@
+//Test 1
+
 //$('.image-wrapper').click(function(e){
     //$('.image-overlay-content').fadeToggle();
 //});
@@ -21,19 +23,83 @@
   //  });
   //}
 
-  if($(window).width() <= 1024) {
-    $('.simple-toggle').click(function(){
-        $('.simple-toggle-content').fadeToggle();
-    });
+// 1 //  if($(window).width() <= 1024) {
+// 1 //    $('.simple-toggle').click(function(){
+// 1 //        $('.simple-toggle-content').fadeToggle();
+// 1 //    });
+
+// 2 // $( ".simple-toggle" ).on({
+// 2 //   click: function() {
+// 2 //     $('.simple-toggle-content').fadeToggle();//$( this ).toggleClass( "active" );
+// 2 //
+// 2 //   },mouseenter: function() {
+// 2 //     $('.simple-toggle-content').fadeToggle();
+// 2 //   }
+// 2 //   ,mouseleave: function() {
+// 2 //     $('.simple-toggle-content').fadeOut();
+// 2 //   }
+// 2 // });
+
+if($(window).width() <= 1024) {
+  $( ".simple-toggle" ).on({
+    click: function() {
+      $('.simple-toggle-content').fadeToggle();//$( this ).toggleClass( "active" );
+
+    }
+    ,mouseleave: function() {
+      $('.simple-toggle-content').fadeOut();
+    }
+   });
+ }
+    else {
+      $('.simple-toggle').hover(function(){
+          $('.simple-toggle-content').fadeToggle(1000);
+       });
+     }
+
+
+// 3 // $( ".simple-toggle" ).on({
+// 3 //  click: function() {
+// 3 //    $('.simple-toggle-content').fadeToggle();//$( this ).toggleClass( "active" );
+// 3 //
+// 3 //  },mouseenter: function() {
+// 3 //    $('.simple-toggle-content').fadeToggle();
+// 3 //  }
+// 3 //  ,mouseleave: function() {
+// 3 //    $('.simple-toggle-content').fadeOut();
+// 3 //  }
+// 3 // });
 
     //$('.simple-toggle').click(function(event){
     //     event.stopPropagation();
     //});
 
+// 1 //  }
+// 1 //  else {
+// 1 //    $('.simple-toggle').hover(function(e){
+// 1 //      e.preventDefault();
+// 1 //        $('.simple-toggle-content').fadeToggle(1000);
+// 1 //    });
+// 1 //  }
+
+//Test 2
+
+//  $( "div.test" ).off({
+//    click: function() {
+//      $( this ).toggleClass( "active" );//$( this ).toggleClass( "active" );
+//    }, click: function() {
+//      $( this ).addClass( "inside" );
+//    }, mouseleave: function() {
+//      $( this ).removeClass( "inside" );
+//    }
+//  });
+
+//Test 3
+
+$( "div.test-2" ).on({
+  click: function() {
+    $( this ).toggleClass( "inside-2" );//$( this ).toggleClass( "active" );
+  },mouseleave: function() {
+    $( this ).removeClass( "inside-2" );
   }
-  else {
-    $('.simple-toggle').hover(function(e){
-      e.preventDefault();
-        $('.simple-toggle-content').fadeToggle(1000);
-    });
-  }
+});
